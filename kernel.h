e8e867860dd33574ed93c4ed3060e094ed31c49d
S Takumi <takumi.shiratori@gmail.com>
2025-01-20 20:05:51 +0900

    11. page table

--- kernel.h
+++ kernel.h
@@ -48,11 +48,21 @@ struct trap_frame {
 #define PROC_RUNNABLE   1 // 実行可能なプロセス
 
 struct process {
-    int pid;              // プロセスID
+    int pid;             // プロセスID
     int state;           // プロセスの状態: PROC_UNUSED または PROC_RUNNABLE
     vaddr_t sp;          // コンテキストスイッチ時のスタックポインタ
+    uint32_t *page_table;
     uint8_t stack[8192]; // カーネルスタック
 };
+
+// page table
+#define SATP_SV32 (1u << 31)
+#define PAGE_V (1 << 0) // 有効化ビット
+#define PAGE_R (1 << 1) // 読み込み可能
+#define PAGE_W (1 << 2) // 書き込み可能
+#define PAGE_X (1 << 3) // 実行可能
+#define PAGE_U (1 << 4) // ユーザーモードでアクセス可能
+
 
 #define PANIC(fmt, ...)                                     \
     do {                                                    \
